660d8791355eec9aabc47ff40df3024efb7c4386
George Nicolas Kontogiorgos <gekontogiorgos@gmail.com>
2024-01-21 23:09:07 +0100

    Implement balance of worked hours

--- gui.py
+++ gui.py
@@ -2,17 +2,20 @@ import tkinter as tk
 from tkinter import messagebox
 import timeregister as tr
 from time import strftime
+import matplotlib.pyplot as plt
 
 def timestamp_decorator(func):
     def wrapper():
         file_path = 'data.csv'
         data = tr.get_data_from_file(file_path)
         try:
-            timestamp = tr.get_current_time()
-            data, result_text = func(data, timestamp)
+            date = tr.get_current_date()
+            time = tr.get_current_time()
+            data, result_text = func(data, date, time)
+            label.after(1000, clock)
             confirmation = messagebox.askquestion("Confirmation", 
                                         "Are you sure you want to register?\n" +
-                                        timestamp)
+                                        date + " " + time)
             if confirmation == 'no':
                 return
             label.config(text=result_text)
@@ -22,20 +25,55 @@ def timestamp_decorator(func):
     return wrapper
 
 @timestamp_decorator
-def clock_in_cmd(data, timestamp):
+def clock_in_cmd(data, date, time):
     if tr.is_last_clock_out(data):
-        data = tr.set_in_timestamp(data, timestamp)
-        return data, "Clock in registered at\n" + timestamp
+        data = tr.set_in_timestamp(data, date, time)
+        return data, "Clock in registered at\n" + date + time
     else:
         raise
 
 @timestamp_decorator
-def clock_out_cmd(data, timestamp):
+def clock_out_cmd(data, date, time):
     if tr.is_last_clock_in(data):
-        data = tr.set_out_timestamp(data, timestamp)
-        return data, "Clock out registered at\n" + timestamp
+        data = tr.set_out_timestamp(data, date, time)
+        return data, "Clock out registered at\n" + date + time
     else:
         raise
+
+def balance_cmd():
+    data = tr.get_data_from_file('data.csv')
+
+    #data = tr.date_range_filter(data, '2024-01-01', '2024-01-09')
+
+    worked_hours = tr.get_worked_hours(data)
+
+    categories = list(worked_hours.keys())
+    values = list(worked_hours.values())
+    plt.figure("Balance", facecolor='black')
+    ax = plt.axes()
+    ax.set_facecolor("black")
+    ax.bar(categories, values, color="green")
+
+    ax.set_xlabel("Date", color="cyan")
+    plt.xticks(color='cyan')
+    
+    ax.set_ylabel("Worked hours", color="cyan")
+    plt.yticks(color='cyan')
+
+    ax.tick_params(axis='x', color='cyan')
+    ax.tick_params(axis='y', color='cyan')
+
+    ax.spines['bottom'].set_color('cyan')
+    ax.spines['left'].set_color('cyan')
+
+    plt.setp( ax.xaxis.get_majorticklabels(), rotation=20, ha="right" )
+
+    plt.tight_layout()
+    plt.show()
+
+def on_close():
+    plt.close('all')
+    window.destroy()
 
 def clock():
     current_time = "Welcome to time register program!\n" + strftime('%Y-%m-%d %H:%M:%S')
@@ -45,9 +83,10 @@ def clock():
 # Create the main window
 window = tk.Tk()
 window.title("Time register")
-window.geometry("360x100")
+window.geometry("360x150")
 window.resizable(False, False)
 window.configure(bg="black")
+window.protocol("WM_DELETE_WINDOW", on_close)
 
 # Create a label
 label = tk.Label(window, 
@@ -74,6 +113,14 @@ clockOutButton = tk.Button(window,
                            height=2)
 
 clockOutButton.place(x=200, y=50)
+
+showBalanceButton = tk.Button(window, 
+                           text="Balance", 
+                           command=balance_cmd,
+                           width=10, 
+                           height=2)
+
+showBalanceButton.place(x=50, y=100)
 
 clock()
 
--- timeregister.py
+++ timeregister.py
@@ -1,5 +1,9 @@
 import pandas as pd
-from time import strftime
+from time import strftime, strptime, mktime, localtime
+from holidays import CountryHoliday
+from datetime import date
+
+timestamp_format = "%Y-%m-%d %H:%M:%S"
 
 def get_data_from_file(file_path):
     try:
@@ -9,26 +13,34 @@ def get_data_from_file(file_path):
     except FileNotFoundError:
         # Handle the case where the file doesn't exist
         print(f"The file '{file_path}' does not exist. Creating an empty DataFrame.")
-        data = {'Timestamp': [],
-            'Type': []}
+        data = {'Date': [],
+                'Time': [],
+                'Type': []}
         df = pd.DataFrame(data)
     return df
 
 def get_current_time():
     # Get current date and time
-    return strftime("%Y-%m-%d %H:%M:%S")
+    return strftime("%H:%M:%S")
+
+def get_current_date():
+    # Get current date and time
+    return strftime("%Y-%m-%d")
 
 def is_last_clock_in(data):
     return data.iloc[-1]['Type'] == 'In'
 
 def is_last_clock_out(data):
-    return data.iloc[-1]['Type'] == 'Out'
+    if data.empty:
+        return True
+    else:
+        return data.iloc[-1]['Type'] == 'Out'
 
 def add_timestamp_entry(entry_type):
     def decorator(func):
-        def wrapper(df, timestamp):
+        def wrapper(df, date, time):
             # Create a new row to append
-            new_data = {'Timestamp': timestamp, 'Type': entry_type}
+            new_data = {'Date': date, 'Time': time, 'Type': entry_type}
 
             # Convert the new row to a DataFrame
             new_row_df = pd.DataFrame([new_data])
@@ -40,12 +52,42 @@ def add_timestamp_entry(entry_type):
     return decorator
 
 @add_timestamp_entry('In')
-def set_in_timestamp(df, timestamp):
+def set_in_timestamp(df, date, time):
     return df
 
 @add_timestamp_entry('Out')
-def set_out_timestamp(df, timestamp):
+def set_out_timestamp(df, date, time):
     return df
 
 def write_data_to_file(df, file_path):
     df.to_csv(file_path, index=False)
+
+def date_range_filter(df, start_date, end_date):
+    return df[(df['Date'] >= start_date) & (df['Date'] <= end_date)]
+
+def get_worked_hours(df):    
+    worked_hours = {}
+    for index, row in df.iterrows():
+        date = row['Date']
+        time = row['Time']
+        entry_type = row['Type']
+        entry_epoch = mktime(strptime(date + " " + time, "%Y-%m-%d %H:%M:%S"))
+
+        if entry_type == 'In':
+            clock_in_time = entry_epoch
+            last_date = date
+        elif entry_type == 'Out':
+            if date == last_date:
+                worked_hours_temp = (entry_epoch - clock_in_time)/3600
+                try:
+                    worked_hours[date] += worked_hours_temp
+                except:
+                    worked_hours[date] = worked_hours_temp
+            else:
+                last_day_out_epoch = mktime(strptime(last_date + " 23:59:59", "%Y-%m-%d %H:%M:%S"))
+                worked_hours[last_date] = (last_day_out_epoch - clock_in_time)/3600
+                new_day_in_epoch = mktime(strptime(date + " 0:0:0", "%Y-%m-%d %H:%M:%S"))
+                worked_hours[date] = (entry_epoch - new_day_in_epoch)/3600
+        else:
+            raise
+    return worked_hours
